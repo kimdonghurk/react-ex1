8490a2e8b3e48fa687fcb1870e39732d1abfdc6b
Kimdonghurk <kdh09151@naver.com>
2024-11-09 17:50:25 +0900

    react ex1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Step01 from './step/step01';
+import Step02 from './step/step02';
+import Step03 from './step/step03';
+import Step04 from './step/step04';
+import Step05 from './step/step05';
+import Step06 from './step/step06';
+import Step07 from './step/step07';
+import Step08 from './step/step08';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* 컴포넌트: 재사용 가능한 코드조각(버튼, 텍스트, 이미지 등등...) */}
+    {/* <Comp /> */}
+    {/* <Comp /> */}
+  {/* <Step01/> */}
+  {/* <Step02/> */}
+  {/* <Step03 /> */}
+  {/* <Step04 title='게시판 제목' content='게시판내용'name='작성자'red='red' /> */}
+  {/* <Step05/> */}
+  {/* <Step06/> */}
+  {/* <Step07/> */}
+  <Step08/>
+    </>
   );
+}
+
+// 작명시 영어 대문자로 시작
+// 함수표현식 함수선언식 둘 다 가능
+
+function Comp(){
+   return <div>컴포넌트</div>
 }
 
 export default App;
